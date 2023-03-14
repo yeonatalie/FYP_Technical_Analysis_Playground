f45504fa864024f77bdcc8de8a1878c89269dee8
yeonatalie <yeonatalie@yahoo.com.sg>
2023-03-14 19:18:36 +0800

    format popup

--- fyp-app/src/components/stockchart/Tutorials/SmaCrossoverTutorial.jsx
+++ fyp-app/src/components/stockchart/Tutorials/SmaCrossoverTutorial.jsx
@@ -140,12 +140,26 @@ function SmaCrossover({data, xScale, yScale, yProfitScale, tutorial, paramData, 
         annotateTradePerformance({svg:svg, data:smaData, xScale:xScale, yScale:yScale, displayTime:3000})
 
         // Pop-up showing trade summary
+        var lastDay = smaData.slice(-1)[0]
+        var strat_returns = (lastDay['strat_gross_cum_ret'] * 100).toFixed(2)
+        var profit = lastDay['strat_gross_profit'].toFixed(2)
+        var num_long_trades = allSignalData.filter(d => d.signal === 1).length
+        var num_short_trades = allSignalData.filter(d => d.signal === -1).length
+
         return (
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                <Modal.Title>Performance</Modal.Title>
+                <Modal.Title style={{fontWeight: "bold"}}>Performance</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Model Returns: </Modal.Body>
+                <Modal.Body style={{fontSize: "18px"}}>
+                    <span style={{fontWeight: "bold"}}>Profit: </span>${profit}
+                    <p style={{margin: "1px"}}></p>
+                    <span style={{fontWeight: "bold"}}>Strategy Returns: </span>{strat_returns}%
+                    <hr class="solid"></hr>
+                    <span style={{fontWeight: "bold"}}>Number of Long Trades: </span>{num_long_trades}
+                    <p style={{margin: "1px"}}></p>
+                    <span style={{fontWeight: "bold"}}>Number of Short Trades: </span>{num_short_trades}
+                </Modal.Body>
             </Modal>
         )
     }
